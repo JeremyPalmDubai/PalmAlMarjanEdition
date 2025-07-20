a6faef77b827b7b2fc2e4e0a2b61dff046822aea
JeremyPalmDubai <jeremy.palmdubai@gmail.com>
2025-07-20 15:10:38 +0400

    Updated AccessibilityProvider.tsx

--- src/components/AccessibilityProvider.tsx
+++ src/components/AccessibilityProvider.tsx
@@ -18,6 +18,7 @@ interface AccessibilityContextType {
 }
 
 const defaultSettings: AccessibilitySettings = {
+  }
   reducedMotion: false,
   highContrast: false,
   fontSize: 'medium',
@@ -131,5 +132,6 @@ export const AccessibilityProvider: React.FC<AccessibilityProviderProps> = ({ ch
     </AccessibilityContext.Provider>
   );
 };
+}
 }
 }
