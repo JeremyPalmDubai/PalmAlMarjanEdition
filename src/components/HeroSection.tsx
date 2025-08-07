a38c183c61fd7ae86d700dcdd76872fe279e19be
JeremyPalmDubai <jeremy.palmdubai@gmail.com>
2025-08-07 12:40:46 +0400

    Updated HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -108,12 +108,7 @@ export const HeroSection: React.FC<HeroSectionProps> = ({ currentLanguage }) => 
         </div>
       </div>
 
-      {/* Tesla-style scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-        <div className="w-6 h-10 border-2 border-white/60 rounded-full flex justify-center">
-          <div className="w-1 h-3 bg-white/60 rounded-full mt-2 animate-pulse"></div>
-        </div>
-      </div>
+
     </section>
   );
 };
