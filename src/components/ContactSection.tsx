4c2390e4f15650ba512d08f5191f4ad0aa317046
JeremyPalmDubai <jeremy.palmdubai@gmail.com>
2025-08-07 13:31:38 +0400

    Fix Tally source parameter

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -153,7 +153,7 @@ export const ContactSection: React.FC<ContactSectionProps> = ({ currentLanguage 
               
               <div className="w-full" style={{ minHeight: '404px' }}>
                 <iframe
-                  src="https://tally.so/embed/mZDk45?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1&source=Invest-AlMarjanIsland.com"
+                  src="https://tally.so/embed/mZDk45?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1&source=investalmarjanisland"
                   loading="lazy"
                   width="100%"
                   height="404"
